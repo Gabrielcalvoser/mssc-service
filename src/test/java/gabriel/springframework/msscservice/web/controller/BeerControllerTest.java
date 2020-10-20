db4ac63a39f2c428534a716e599c32a0d3faa85d
GabrielCalvoSerrano <64129705+GabrielCalvoSerrano@users.noreply.github.com>
2020-10-20 14:32:51 +0200

    adding controller test - closes #4

---
+++ src/test/java/gabriel/springframework/msscservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,61 @@
+package gabriel.springframework.msscservice.web.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gabriel.springframework.msscservice.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void getBeerById() throws Exception {
+
+        //given(beerService.getById(any(), anyBoolean())).willReturn(getValidBeerDto());
+
+        mockMvc.perform(get("/api/v1/beer/" +  UUID.randomUUID().toString())
+        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void saveNewBeer() throws Exception {
+
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoToJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(post("/api/v1/beer/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoToJson))
+                .andExpect(status().isCreated());
+
+    }
+
+    @Test
+    void updateBeerById() throws Exception{
+
+        BeerDto beerDto = BeerDto.builder().build();
+        String beerDtoToJson = objectMapper.writeValueAsString(beerDto);
+
+        mockMvc.perform(put("/api/v1/beer/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoToJson))
+                .andExpect(status().isCreated());
+    }
+}
