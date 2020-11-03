71a555d6d450686d64118fd34bbfc459e3864154
GabrielCalvoSerrano <64129705+GabrielCalvoSerrano@users.noreply.github.com>
2020-11-03 18:18:29 +0100

    fixing test #closes 11

--- src/test/java/gabriel/springframework/msscservice/web/controller/BeerControllerTest.java
+++ src/test/java/gabriel/springframework/msscservice/web/controller/BeerControllerTest.java
@@ -3,12 +3,14 @@ package gabriel.springframework.msscservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gabriel.springframework.msscservice.web.model.BeerDto;
+import gabriel.springframework.msscservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -37,7 +39,7 @@ class BeerControllerTest {
     @Test
     void saveNewBeer() throws Exception {
 
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeer();
         String beerDtoToJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(post("/api/v1/beer/")
@@ -50,12 +52,21 @@ class BeerControllerTest {
     @Test
     void updateBeerById() throws Exception{
 
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeer();
         String beerDtoToJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(put("/api/v1/beer/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoToJson))
                 .andExpect(status().isCreated());
+    }
+
+    BeerDto getValidBeer(){
+        return BeerDto.builder()
+                .beerName("Cruzcampo")
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(new BigDecimal("2.50"))
+                .upc(123214343L)
+                .build();
     }
 }
