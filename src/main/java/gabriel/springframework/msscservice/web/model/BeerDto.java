fc65bc0f84a2aba734c04431c95fa3634d90632d
GabrielCalvoSerrano <64129705+GabrielCalvoSerrano@users.noreply.github.com>
2020-11-17 14:25:29 +0100

    clase 114

--- src/main/java/gabriel/springframework/msscservice/web/model/BeerDto.java
+++ src/main/java/gabriel/springframework/msscservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package gabriel.springframework.msscservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +19,8 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    static final long serialVersionUID = -5815566940065181210L;
+
     @Null
     private UUID id;
 
@@ -25,9 +28,11 @@ public class BeerDto {
     private Integer version;
 
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
@@ -37,14 +42,13 @@ public class BeerDto {
     private BeerStyleEnum beerStyle;
 
     @NotNull
-    @Positive
-    private Long upc;
+    private String upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
 
     private Integer quantityOnHand;
-
 
 }
